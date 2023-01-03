b3204ea5db95d7a7caaa5e9220528f92eb194bc1
Derek Colley <derek@colley.cc>
2023-01-03 10:56:23 +0000

    update to use BullMQAdapter

--- bullmq/server.js
+++ bullmq/server.js
@@ -4,6 +4,7 @@ dotenv.config()
 import express from 'express'
 import { Queue, Worker } from 'bullmq'
 import { ExpressAdapter, createBullBoard, BullAdapter, BullMQAdapter } from '@bull-board/express'
+import axios from 'axios'
 
 import { asyncForEach } from './workers/utils.js'
 import { f_1kv_candidates_update } from './workers/1kv-candidates-update.js'
@@ -14,7 +15,6 @@ import { f_w3f_nominators_update } from './workers/w3f-nominators-update.js'
 import { f_w3f_pools_update } from './workers/w3f-pools-update.js'
 import { f_w3f_validator_location_stats_update } from './workers/w3f-validator-location-stats-update.js'
 import { f_w3f_validators_update } from './workers/w3f-validators-update.js'
-import axios from 'axios'
 
 const env = process.env
 const chains = ['kusama', 'polkadot']
@@ -139,14 +139,14 @@ async function clearQueue (jobname) {
   // const queueMQ = new QueueMQ()
   const { setQueues, replaceQueues } = createBullBoard({
     queues: [
-      new BullAdapter(q_1kv_candidates_update, { readOnlyMode: false }),
-      new BullAdapter(q_1kv_nominations_update, { readOnlyMode: false }),
-      new BullAdapter(q_1kv_nominators_update, { readOnlyMode: false }),
-      new BullAdapter(q_w3f_exposures_update, { readOnlyMode: false }),
-      new BullAdapter(q_w3f_nominators_update, { readOnlyMode: false }),
-      new BullAdapter(q_w3f_pools_update, { readOnlyMode: false }),
-      new BullAdapter(q_w3f_validator_location_stats_update, { readOnlyMode: false }),
-      new BullAdapter(q_w3f_validators_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_1kv_candidates_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_1kv_nominations_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_1kv_nominators_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_w3f_exposures_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_w3f_nominators_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_w3f_pools_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_w3f_validator_location_stats_update, { readOnlyMode: false }),
+      new BullMQAdapter(q_w3f_validators_update, { readOnlyMode: false }),
     ],
     serverAdapter: serverAdapter,
   })
