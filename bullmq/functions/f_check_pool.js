6a8c7bed8504e5ec2e1cba91b082bce9322ca1b2
Derek Colley <derek@metaspan.com>
2023-10-21 13:10:44 +0000

    remove telegram token

--- bullmq/functions/f_check_pool.js
+++ bullmq/functions/f_check_pool.js
@@ -1,14 +1,14 @@
-import Telegram from 'telegraf/telegram';
+// import Telegram from 'telegraf/telegram';
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import axios from 'axios';
 
 // todo: move to config
-const pools = {
-  polkadot: '13UVJyLnbVp8c4FQeiGDrYotodEcyAzE8tipNEMc61UBJAH4', // pool stash
-  kusama: 'F3opxRbN5ZavB4LTn2JaUnScQc7G7G177CPnqjBpa9F9Gdr',
-}
-const TELEGRAM_BOT_TOKEN = '5634840351:AAGx6SekIhkz9ExEr_TFAjRH67b2_iyorWI';
-const TELEGRAM_CHAT_ID = '651499652';
+// const pools = {
+//   polkadot: '13UVJyLnbVp8c4FQeiGDrYotodEcyAzE8tipNEMc61UBJAH4', // pool stash
+//   kusama: 'F3opxRbN5ZavB4LTn2JaUnScQc7G7G177CPnqjBpa9F9Gdr',
+// }
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 export async function f_check_pool (job) {
 
@@ -34,15 +34,10 @@ export async function f_check_pool (job) {
   // console.log(activeValidators);
 
   const hasActiveValidator = nominatedValidators.some(validator => activeValidators.includes(validator));
-  console.log(`${poolAddress} has Active Validator: ${hasActiveValidator}`);
-
-  // // send notification to telegram
-  // const telegram = new Telegram(TELEGRAM_BOT_TOKEN);
-  // const message = `${poolAddress} has Active Validator: ${hasActiveValidator}`;
-  // telegram.sendMessage(TELEGRAM_CHAT_ID, message);
+  job.log(`${poolAddress} has active validator: ${hasActiveValidator}`);
 
   // send notification to telegram
-  const message = `${poolAddress} has Active Validator: ${hasActiveValidator}`;
+  const message = `${chainId} pool active: ${hasActiveValidator}`;
   const respo = await axios.get(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${message}`);
   if(respo.data?.ok) {
     job.log(`Telegram notification sent`)
